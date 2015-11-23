59fc20d2934edaf4454bb79fc0edfe6dd0db1578
Jasper Van Proeyen <trianglejuice@mac.com>
2015-11-23 15:02:43 +0100

    Added top nav check + speed parameter

--- src/scripts/services/tink-anchor-scroll-angular.js
+++ src/scripts/services/tink-anchor-scroll-angular.js
@@ -5,13 +5,13 @@
   } catch (e) {
     module = angular.module('tink.anchorscroll', []);
   }
-  module.service('tinkAnchorScroll', [function () {
+  module.service('tinkAnchorScroll', ['$timeout', function ($timeout) {
 
     /**
      * Check the API down below
      */
 
-    function _scrollTo(id) {
+    function _scrollTo(id, speed) {
 
       /**
        * Helper functions
@@ -20,25 +20,30 @@
         return window.pageYOffset;
       }
 
-      function elmYPosition(id) {
-        var elm = document.getElementById(id);
+      function elmYPosition() {
         var y = elm.offsetTop;
         var node = elm;
+        var padding = parseFloat($('body').css('padding-top')) || 0;
         while (node.offsetParent && node.offsetParent !== document.body) {
           node = node.offsetParent;
           y += node.offsetTop;
-        } return y;
+        } return (y - padding);
       }
 
-
+      var elm = document.getElementById(id);
       var startY = currentYPosition();
-      var stopY = elmYPosition(id);
+      var stopY = elmYPosition();
       var distance = stopY > startY ? stopY - startY : startY - stopY;
       if (distance < 100) {
         scrollTo(0, stopY); return;
       }
-      var speed = Math.round(distance / 100);
-      if (speed >= 20) { speed = 20; }
+
+      // Set speed
+      if (typeof speed === 'undefined') {
+        speed = Math.round(distance / 100);
+        if (speed >= 15) { speed = 15; }
+      }
+
       var step = Math.round(distance / 25);
       var leapY = stopY > startY ? startY + step : startY - step;
       var timer = 0;
@@ -52,6 +57,9 @@
         setTimeout('window.scrollTo(0, '+leapY+')', timer * speed);
         leapY -= step; if (leapY < stopY) { leapY = stopY; } timer++;
       }
+      $timeout(function() {
+        elm.focus();
+      });
     }
 
 
