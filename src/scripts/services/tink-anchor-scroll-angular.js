ed033e7f1a21e2bab7feffadc6ee4074a659a86a
Jasper Van Proeyen <trianglejuice@mac.com>
2015-11-23 10:37:43 +0100

    First version scroll service

--- src/scripts/services/tink-anchor-scroll-angular.js
+++ src/scripts/services/tink-anchor-scroll-angular.js
@@ -7,12 +7,60 @@
   }
   module.service('tinkAnchorScroll', [function () {
 
-    function _test() {
-      console.log('srvc');
+    /**
+     * Check the API down below
+     */
+
+    function _scrollTo(id) {
+
+      /**
+       * Helper functions
+       */
+      function currentYPosition() {
+        return window.pageYOffset;
+      }
+
+      function elmYPosition(id) {
+        var elm = document.getElementById(id);
+        var y = elm.offsetTop;
+        var node = elm;
+        while (node.offsetParent && node.offsetParent !== document.body) {
+          node = node.offsetParent;
+          y += node.offsetTop;
+        } return y;
+      }
+
+
+      var startY = currentYPosition();
+      var stopY = elmYPosition(id);
+      var distance = stopY > startY ? stopY - startY : startY - stopY;
+      if (distance < 100) {
+        scrollTo(0, stopY); return;
+      }
+      var speed = Math.round(distance / 100);
+      if (speed >= 20) { speed = 20; }
+      var step = Math.round(distance / 25);
+      var leapY = stopY > startY ? startY + step : startY - step;
+      var timer = 0;
+      if (stopY > startY) {
+        for ( var i=startY; i<stopY; i+=step ) {
+          setTimeout('window.scrollTo(0, '+leapY+')', timer * speed);
+          leapY += step; if (leapY > stopY) { leapY = stopY; } timer++;
+        } return;
+      }
+      for ( var j=startY; j>stopY; j-=step ) {
+        setTimeout('window.scrollTo(0, '+leapY+')', timer * speed);
+        leapY -= step; if (leapY < stopY) { leapY = stopY; } timer++;
+      }
     }
 
+
+    /**
+     * API
+     */
+
     return {
-      test: _test
+      scrollTo: _scrollTo
     };
 
   }]);
